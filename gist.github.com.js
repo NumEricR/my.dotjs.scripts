73a0980217db15adf0494a273adcd7a4e301049c
NumEricR <eric.github@gmail.com>
2011-08-23 12:39:11 +0200

    Added gist.github.com.js

---
+++ gist.github.com.js
@@ -0,0 +1,12 @@
+/*
+ * Created by NumEricR - 23-08-2011
+ * Successfully tested on FF 6 / Mac OS
+ */
+
+function modifyHeader() {
+  $('#header .avatarname a:first-child').hide();
+  $('#top_search_form input.search').css('font-size', '12px');
+}
+
+
+modifyHeader();
