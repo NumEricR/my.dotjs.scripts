8e4db7169e7ffa1dd738b72863d35ec44a393211
NumEricR <eric.github@gmail.com>
2011-12-19 23:35:42 +0100

    Change delicious.com footer

--- delicious.com.js
+++ delicious.com.js
@@ -3,6 +3,7 @@
  * Successfully tested on FF 8 / Mac OS
  */
 
+/* Top content */
 $('.tabsPage').css({
   'margin-top': '25px'
 }).children('.tabs').css({
@@ -14,3 +15,18 @@ $('.tabsPage').css({
   'padding': '0 10px'
 });
 $('#user_profile .sidebar').css('margin-top', '25px');
+
+
+/* Footer */
+$('#main').css('padding-bottom', '20px');
+$('#footer').css({
+  'padding': '20px 0'
+});
+$('#footer a').hover(
+  function() {
+    $(this).css('color', '#2160ba');
+  },
+  function() {
+    $(this).css('color', '#6e9cd8');
+  }
+);
