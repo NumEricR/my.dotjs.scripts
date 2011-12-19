3ad1d9ac0d1a568a0dfc56bc3d177673fdb05050
NumEricR <eric.github@gmail.com>
2011-12-19 22:17:44 +0100

    Change tabs of new delicious.com design

---
+++ delicious.com.js
@@ -0,0 +1,15 @@
+/*
+ * Created by NumEricR - 19-12-2011
+ * Successfully tested on FF 8 / Mac OS
+ */
+
+$('.tabsPage').css({
+  'margin-top': '25px'
+}).children('.tabs').css({
+  'height': '30px',
+  'top': '-30px'
+}).children('a').css({
+  'line-height': '30px',
+  'min-width': '30px',
+  'padding': '0 10px'
+});
