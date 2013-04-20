470d548e458df022b3896d3fad7f11ea0da807a4
NumEricR <eric.github@gmail.com>
2013-04-20 17:27:15 +0200

    Customize Google results page

---
+++ .js/google.com.js
@@ -0,0 +1,18 @@
+/*
+ * Created by NumEricR
+ * See: https://github.com/NumEricR/my.dotjs.scripts
+ *
+ * License: WTFPL - Do What The Fuck You Want To Public License
+ * https://github.com/NumEricR/my.dotjs.scripts/blob/master/License.txt
+ */
+
+/*  */
+var toolsMenu = document.getElementById('hdtbMenus');
+if (toolsMenu != null) {
+	toolsMenu.className = 'hdtb-td-o';
+}
+/*  */
+var appbar = document.getElementById('appbar');
+if (appbar != null) {
+	appbar.className = 'hdtb-td-o';
+}
