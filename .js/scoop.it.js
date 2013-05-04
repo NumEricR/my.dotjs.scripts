035132a59abc3f54778cead4b843ce09db7500bd
NumEricR <eric.github@gmail.com>
2013-05-04 10:44:35 +0200

    Add js for scoop.it to avoid target attribute on external links

---
+++ .js/scoop.it.js
@@ -0,0 +1,10 @@
+/*
+ * Created by NumEricR
+ * See: https://github.com/NumEricR/my.dotjs.scripts
+ *
+ * License: WTFPL - Do What The Fuck You Want To Public License
+ * https://github.com/NumEricR/my.dotjs.scripts/blob/master/License.txt
+ */
+
+/* Remove target attribute on external links */
+$('.postTitleView a').removeAttr('target');
