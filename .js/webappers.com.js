611b676437207407bd1b97bef0269ba5f1d7e5f2
NumEricR <eric.github@gmail.com>
2013-06-11 10:59:53 +0200

    Remove links' target attribute in posts content of WebAppers.com

---
+++ .js/webappers.com.js
@@ -0,0 +1,9 @@
+/*
+ * Created by NumEricR
+ * See: https://github.com/NumEricR/my.dotjs.scripts
+ *
+ * License: WTFPL - Do What The Fuck You Want To Public License
+ * https://github.com/NumEricR/my.dotjs.scripts/blob/master/License.txt
+ */
+
+$('.post_content a').removeAttr('target');
